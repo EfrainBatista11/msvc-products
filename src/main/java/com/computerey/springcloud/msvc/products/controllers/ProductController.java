cf7ace97a50ab9c468e5d1d5c85a02d110fb3fc7
Efraín Batista <92494674+EfrainBatista11@users.noreply.github.com>
2026-02-15 11:40:50 -0500

    Sección 4 c Spring Cloud Gateway

--- src/main/java/com/computerey/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/computerey/springcloud/msvc/products/controllers/ProductController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.computerey.springcloud.msvc.products.entities.Product;
@@ -14,7 +13,6 @@ import com.computerey.springcloud.msvc.products.services.ProductService;
 
 
 @RestController
-@RequestMapping("/api/products")
 public class ProductController {
     
     private final ProductService service;
